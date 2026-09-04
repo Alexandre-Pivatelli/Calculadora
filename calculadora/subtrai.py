9473a14ef158f8a13fbe3ae517799c950de8a1d6
BiaToledano <biatoledano@gmail.com>
2026-09-04 13:27:56 +0000

    atualizei o subtrai

--- calculadora/subtrai.py
+++ calculadora/subtrai.py
@@ -1,8 +1,4 @@
 def subtrai (x,y)
     sub= x-y
     return sub 
-
-x= float(input('Digite o orimeiro numero:'))
-y= float(input('Digite o segundo numero:'))
-
-print(subtrai(x,y))
+#print(subtrai(10,5))
